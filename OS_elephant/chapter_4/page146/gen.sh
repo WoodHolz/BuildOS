e5d08934d7df03aab1373f5caface06915967ab1
WoodHolz <1535980162@qq.com>
2023-09-09 18:38:40 +0800

    OS_elephant_C4

---
+++ OS_elephant/chapter_4/page146/gen.sh
@@ -0,0 +1,6 @@
+if [ ! -d "./build" ]; then
+      mkdir build
+fi
+bximage_gdb -hd -mode="flat" -size=3 -q OS.img
+nasm -I include/ -o ./build/mbr_toloader.bin mbr_toloader.S && dd if=./build/mbr_toloader.bin of=./OS.img bs=512 count=1  conv=notrunc
+nasm -I include/ -o ./build/16push.bin 16push.S  && dd if=./build/16push.bin of=./OS.img bs=512 count=1 seek=2 conv=notrunc
