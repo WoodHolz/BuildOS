1fde9c8c709900248570ff2793eb6b52b3d76007
WoodHolz <1535980162@qq.com>
2023-09-17 21:16:12 +0800

    OS_elephant_C4_protectmode

---
+++ OS_elephant/chapter_4/a/boot/gen.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ ! -d "./out" ]; then
+      mkdir out
+OS
+fi
+if [ -e "OS.img" ]; then
+      rm -rf OS.img  
+fi
+bximage -hd -mode="flat" -size=3 -q OS.img
+nasm -I include/ -o ./out/mbr.bin mbr.S && dd if=./out/mbr.bin of=./OS.img bs=512 count=1  conv=notrunc
+nasm -I include/ -o ./out/loader.bin loader.S && dd if=./out/loader.bin of=./OS.img bs=512 count=4 seek=2 conv=notrunc
