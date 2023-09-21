521ce6ec05e37bf4c361a65333a765f24ee22e1c
WoodHolz <1535980162@qq.com>
2023-09-21 17:42:05 +0800

    update tool

--- OS_elephant/tool/gen.sh
+++ OS_elephant/tool/gen.sh
@@ -1,9 +1,12 @@
+#!/bin/bash
 if [ ! -d "./build" ]; then
-    mkdir build
+      mkdir build
 fi
 if [ -e "OS.img" ]; then
-    rm -rf OS.img  
+      rm -rf OS.img  
 fi
-bximage_gdb -hd -mode="flat" -size=3 -q OS.img
-nasm -I include/ -o ./build/mbr_toloader.bin mbr_toloader.S && dd if=./build/mbr_toloader.bin of=./OS.img bs=512 count=1  conv=notrunc
-nasm -I include/ -o ./build/16push.bin 16push.S  && dd if=./build/16push.bin of=./OS.img bs=512 count=1 seek=2 conv=notrunc
+bximage -hd -mode="flat" -size=3 -q OS.img
+nasm -I include/ -o ./build/mbr.bin mbr.S
+nasm -I include/ -o ./build/loader.bin loader.S
+dd if=./build/mbr.bin of=./OS.img bs=512 count=1  conv=notrunc
+dd if=./build/loader.bin of=./OS.img bs=512 count=4 seek=2 conv=notrunc
