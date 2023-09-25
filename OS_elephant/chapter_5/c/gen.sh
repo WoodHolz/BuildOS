62837f65685979768cd6c26e5d87656521c2e4bc
WoodHolz <1535980162@qq.com>
2023-09-25 21:08:34 +0800

    fix

--- OS_elephant/chapter_5/c/gen.sh
+++ OS_elephant/chapter_5/c/gen.sh
@@ -14,6 +14,6 @@ fi
 bximage -hd -mode="flat" -size=3 -q OS.img
 nasm -I ./boot/include/ -o ./build/boot/mbr.bin ./boot/mbr.S && dd if=./build/boot/mbr.bin of=./OS.img bs=512 count=1  conv=notrunc
 nasm -I ./boot/include/ -o ./build/boot/loader.bin ./boot/loader.S && dd if=./build/boot/loader.bin of=./OS.img bs=512 count=4 seek=2 conv=notrunc
-gcc-4.4 -c -o build/kernel/main.o kernel/main.c
-ld build/kernel/main.o -Ttext 0xc0001500 -e main -o build/kernel/kernel.bin && \
+gcc -c -o build/kernel/main.o kernel/main.c -m32
+ld build/kernel/main.o -Ttext 0xc0001500 -e main -o build/kernel/kernel.bin -m elf_i386 && \
     dd if=./build/kernel/kernel.bin of=./OS.img bs=512 count=200 seek=9 conv=notrunc
